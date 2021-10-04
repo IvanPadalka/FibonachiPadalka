2628139e74e804ceb490af0c99d25d44f944be22
Joon699 <boichuko70@ukr.net>
2021-10-04 15:31:16 +0300

    Minor fixes

--- src/main/java/task_2/Main.java
+++ src/main/java/task_2/Main.java
@@ -1,8 +1,6 @@
 package task_2;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class Main {
@@ -26,27 +24,53 @@ public class Main {
             switch (choice) {
 
                 case 1:
-                    cars.add(new BMW("Sedan","A280","black"));
+
+                    System.out.println("Please write type of the car:");
+                    String typeName = input.nextLine();
+
+                    System.out.println("Please write model of the car:");
+                    String modelName = input.nextLine();
+
+                    System.out.println("Please write brand of the car:");
+                    String brandName = input.nextLine();;
+
+                    System.out.println("Please write color of the car:");
+                    String colorName = input.nextLine();
+
+                    System.out.println("Please write speed of the car:");
+                    int speed = input.nextInt();
+
+                    cars.add(new BMW(typeName, modelName, brandName, colorName, speed));
                     break;
 
                 case 2:
-                    cars.remove(1);
+
+                    System.out.println("Please write number of object to remove:");
+                    int choiceRemove = input.nextInt();
+
+                    cars.remove(choiceRemove);
                     break;
 
                 case 3:
+
                     cars.stream()
-                        .forEach(System.out::print);
+                            .forEach(System.out::print);
+
                     break;
 
                 case 4:
-                    cars.stream().sorted();
+
+                    Collections.sort(cars);
                     break;
 
                 case 5:
+
                     System.out.println("Exiting Program...");
                     System.exit(0);
+
                     break;
                 default:
+
                     System.out.println("This is not a valid Menu Option! Please Select Another");
                     break;
 
@@ -78,13 +102,21 @@ interface Vehicle {
 
 }
 
-abstract class Car implements Vehicle, Alarm {
+abstract class Car implements Vehicle, Alarm, Comparable<Car> {
 
     protected String type;
     protected String model;
     protected String brand;
     protected String color;
     protected int speed;
+
+    public Car(String type, String model, String brand, String color, int speed) {
+        this.type = type;
+        this.model = model;
+        this.brand = brand;
+        this.color = color;
+        this.speed = speed;
+    }
 
     public int increaseSpeed(int increment) {
         this.speed = this.speed + increment;
@@ -96,14 +128,34 @@ abstract class Car implements Vehicle, Alarm {
         return this.speed;
     }
 
+    @Override
+    public String toString() {
+        return "Car{" +
+                "type='" + type + '\'' +
+                ", model='" + model + '\'' +
+                ", brand='" + brand + '\'' +
+                ", color='" + color + '\'' +
+                ", speed=" + speed +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        int result;
+        if (this.speed > o.speed) {
+            result = 1;
+        } else {
+            result = 0;
+        }
+        return result;
+
+    }
 }
 
 class BMW extends Car {
 
-    BMW(String type, String model, String color) {
-        this.type = type;
-        this.model = model;
-        this.color = color;
+    public BMW(String type, String model, String brand, String color, int speed) {
+        super(type, model, brand, color, speed);
     }
 
     public String getBrand() {
@@ -124,10 +176,10 @@ class BMW extends Car {
 }
 
 class Mercedes extends Car {
-    Mercedes(String type, String model, String color) {
-        this.type = type;
-        this.model = model;
-        this.color = color;
+
+
+    public Mercedes(String type, String model, String brand, String color, int speed) {
+        super(type, model, brand, color, speed);
     }
 
     public String getBrand() {
